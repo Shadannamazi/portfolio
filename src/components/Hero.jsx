8e2d9653ccda83e9c1147032c8d68665ce6adf8c
Shadannamazi <89245846+Shadannamazi@users.noreply.github.com>
2023-08-10 18:24:53 -0700

    made computer rotate

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
-
+import { slideIn } from "../utils/motion";
 const Hero = () => {
   return (
     <section className={`relative w-full h-screen mx-auto`}>
@@ -26,11 +26,12 @@ const Hero = () => {
       </div>
 
       <ComputersCanvas />
-
+      
       <div className='absolute xs:bottom-5 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
             <motion.div
+              
               animate={{
                 y: [0, 24, 0],
               }}
